79ec8d500921ac83e515f04d65f59b8db5d8fbdf
Sunbo2017 <962381354@qq.com>
2018-03-08 17:33:20 +0800

    递归与排序练习

---
+++ web/src/test/java/com/sunbo/test/sort/SortTest.java
@@ -0,0 +1,98 @@
+package com.sunbo.test.sort;
+
+import org.junit.Test;
+
+/**
+ * @author sunboyan
+ *         Date 2018/3/8
+ *         Description java排序算法总结
+ */
+public class SortTest {
+
+    /**
+     * 直接插入排序
+     */
+    @Test
+    public void directInsertSort() {
+        int[] a = {49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 1};
+        System.out.println("排序之前：");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        //直接插入排序
+        for (int i = 1; i < a.length; i++) {
+            //待插入元素
+            int temp = a[i];
+            int j;
+            for (j = i - 1; j >= 0; j--) {
+                //将大于temp的往后移动一位
+                if (a[j] > temp) {
+                    a[j + 1] = a[j];
+                } else {
+                    break;
+                }
+            }
+            a[j + 1] = temp;
+        }
+        System.out.println();
+        System.out.println("排序之后：");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+    /**
+     * 选择排序
+     */
+    @Test
+    public void chooseSort(){
+        int[] a = {49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 1};
+        System.out.println("排序之前：");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        int temp;
+        for (int i = 0; i < a.length; i++){
+            for (int j = i + 1; j < a.length; j++){
+                if (a[i] > a[j]){
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("排序之后：");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+    /**
+     * 冒泡排序
+     */
+    @Test
+    public void bubbleSort(){
+        int[] a = {49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 1};
+        System.out.println("排序之前：");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        int temp;
+        for (int i = 0; i < a.length; i++){
+            //这里-i主要是每遍历一次都把最大的i个数挑出到最后，没有必要再替换了
+            for (int j = 0; j < a.length - i -1; j++){
+                if (a[j] > a[j + 1]){
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("排序之后：");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
